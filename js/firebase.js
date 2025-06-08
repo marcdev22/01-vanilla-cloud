954dad71000064d170b91047bdf1c70fb2a8891f
marcdev22 <markoantonioec2000@gmail.com>
2025-06-07 21:57:24 -0600

    ADD: Initialized Firebase logic and connection

---
+++ js/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-storage.js";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA4c-JXnnG8M14dD-cEFVQmkF07UG5h4X8",
+    authDomain: "vanillajs-cloud.firebaseapp.com",
+    projectId: "vanillajs-cloud",
+    storageBucket: "vanillajs-cloud.firebasestorage.app",
+    messagingSenderId: "254754454446",
+    appId: "1:254754454446:web:b56468e55251bf832ae63f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
